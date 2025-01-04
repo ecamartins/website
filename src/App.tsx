92d41b60095dbcb68b64435259a65acc3b06b230
ecamartins <65371150+ecamartins@users.noreply.github.com>
2025-01-03 16:51:50 -0800

    feat: Update theme colours and add particle background to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,39 @@ import { useState } from 'react';
 import './App.scss'
 import { HomePage } from './HomePage';
 import { NavBar } from './NavBar'
+import ParticlesComponent from './ParticlesBackground';
 import { Projects } from './Projects';
 import { AppPageType } from './utils/common';
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#64b5f6',
-        light: '#FFFBFF',
-        contrastText: '#0d47a1',
-      }
-    },
-    typography: {
-      fontFamily: "Encode Sans Condensed"
-    }
-  });
-  const [page, setPage] = useState<AppPageType>("HOME");
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#000000',
+                light: '#FFFBFF',
+                contrastText: '#891131',
+            }
+        },
+        typography: {
+            fontFamily: "Encode Sans Condensed"
+        }
+    });
+    const [page, setPage] = useState<AppPageType>("HOME");
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="website-wrapper">
-        <NavBar onNavBarClick={(page: AppPageType) => setPage(page)} />
-        <div className="content">
-          <HomePage show={page === "HOME"} />
-          <Projects show={page === "PROJECTS"} />
-        </div>
-      </div>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="website-wrapper">
+                <NavBar onNavBarClick={(page: AppPageType) => setPage(page)} />
+                <div className="content">
+                    {page == "HOME" && <div className="particle-background">
+                        <ParticlesComponent />
+                    </div>}
+                    <HomePage show={page === "HOME"} />
+                    <Projects show={page === "PROJECTS"} />
+                </div>
+            </div>
+        </ThemeProvider>
+    )
 }
 
 export default App
